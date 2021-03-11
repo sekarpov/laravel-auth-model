f941b72ad03cac770719a28fe1a2e3c3d815449a
sekarpov <sergo8ck@gmail.com>
2021-03-12 01:00:01 +0300

    User

---
+++ app/Http/Requests/Admin/Users/UpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Admin\Users;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,id,' . $this->user->id
+        ];
+    }
+}
