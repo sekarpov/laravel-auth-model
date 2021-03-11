5b202e1be309ad4c5bb5ca33cb29f2ca41028aa4
sekarpov <sergo8ck@gmail.com>
2021-03-12 01:22:13 +0300

    Factories and Seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,12 +22,15 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $active = $this->faker->boolean;
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'verify_token' => $active ? null : Str::uuid(),
+            'status' => $active ? User::STATUS_ACTIVE : User::STATUS_WAIT
         ];
     }
 
